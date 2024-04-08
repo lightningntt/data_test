f32c95d457ce0b66794005e5c55eaac8eb9cdb51
Trung <trung191997@gmail.com>
2024-04-08 09:27:38 +0700

    test

--- src/util/extract_snowflake.py
+++ src/util/extract_snowflake.py
@@ -1,61 +1,8 @@
 import os
 import traceback
-from unittest import expectedFailure
 import snowflake.connector as snow_connect
 from snowflake.connector import DictCursor
-import pandas as pd
 
-
-import boto3
-import json
-from botocore.exceptions import ClientError
-
-
-def _retrieve_secret(secret_id: str) -> dict:
-    """
-    Retrieve secrets from the Secrets Manager given the arn
-
-    :param secret_id: str, arn of the Secrets
-    :return: json of the Secrets
-    """
-    try:
-        session = boto3.session.Session()
-        region_name = session.region_name
-
-        client = session.client(
-            service_name='secretsmanager',
-            region_name=region_name
-        )
-
-        credentials = client.get_secret_value(SecretId=secret_id)
-
-        if 'SecretString' in credentials:
-            credentials = credentials['SecretString']
-
-        credentials = json.loads(credentials)
-
-        return credentials
-
-    except Exception as e:
-        raise e
-        
-NEW_RELIC_CREDENTIALS_SECRET = _retrieve_secret("new-relic-credentials")
-SNOWFLAKE_CREDENTIALS_SECRET  = _retrieve_secret("snowflake-credential")
-
-print('check credential:\n')
-print(NEW_RELIC_CREDENTIALS_SECRET)
-print(SNOWFLAKE_CREDENTIALS_SECRET)
-
-os.environ["NEW_RELIC_TOKEN"]     = NEW_RELIC_CREDENTIALS_SECRET["token"]
-
-os.environ["SNOWFLAKE_USER"]      = 'TRUNGNTINNOVATION' #SNOWFLAKE_CREDENTIALS_SECRET["user_account"]
-os.environ["SNOWSQL_PWD"]         = 'cbyiXiigPNDnZ6_' #SNOWFLAKE_CREDENTIALS_SECRET["password"]
-os.environ["SNOWFLAKE_ACCOUNT"] = 'ejb06349.us-east-1' #SNOWFLAKE_CREDENTIALS_SECRET["account"]
-os.environ["SNOWFLAKE_ROLE"] = 'ACCOUNTADMIN' #SNOWFLAKE_CREDENTIALS_SECRET["role"]
-os.environ["SNOWFLAKE_WAREHOUSE"] = 'COMPUTE_WH' #SNOWFLAKE_CREDENTIALS_SECRET["warehouse"]
-os.environ["SNOWFLAKE_DATABASE"] = 'DATA_TESTING' #SNOWFLAKE_CREDENTIALS_SECRET["database"]
-os.environ["SNOWFLAKE_REGION"] = 'us-east-1' #SNOWFLAKE_CREDENTIALS_SECRET["region"]
-os.environ["SNOWFLAKE_AUTHENTICATOR"] = 'https://ejb06349.us-east-1.snowflakecomputing.com' #SNOWFLAKE_CREDENTIALS_SECRET["authenticator"]
 
 os.environ["SNOWFLAKE_SCHEMA"]= 'NEWRELIC'
 
@@ -63,9 +10,7 @@ class SnowflakeConnector:
     
     def __init__(self):
         """"Create the connection to snowflake"""
-        #self.connector = None
-        # while True:
-            # try:
+        
         self.connector = snow_connect.connect(
                 user = os.environ["SNOWFLAKE_USER"],
                 account=os.environ["SNOWFLAKE_ACCOUNT"],
